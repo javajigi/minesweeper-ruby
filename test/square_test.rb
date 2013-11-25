b58fecee31cd977d62de006942e68959f43c1f50
javajigi <javajigi@gmail.com>
2013-11-25 15:05:31 +0900

    test에서 setup을 사용하도록 수정함.

--- test/square_test.rb
+++ test/square_test.rb
@@ -3,46 +3,47 @@ require './helper'
 require 'square'
 
 class SquareTest < Test::Unit::TestCase
+  @square = nil
+
+  def setup
+    @square = Square.new
+  end
+
   def test_open_square
-    square = Square.new
-    assert_equal(false, square.opened?)
-    square.open()
-    assert_equal(true, square.opened?)
+    assert_equal(false, @square.opened?)
+    @square.open()
+    assert_equal(true, @square.opened?)
   end
 
   def test_put_mine
-    square = Square.new
-    assert_equal(false, square.mine?)
-    square.put_mine
-    assert_equal(true, square.mine?)
+    assert_equal(false, @square.mine?)
+    @square.put_mine
+    assert_equal(true, @square.mine?)
   end
 
   def test_win_when_open_not_mine
-    square = Square.new
-    assert(!square.win?)
-    square.open
-    assert(square.win?)
+    assert(!@square.win?)
+    @square.open
+    assert(@square.win?)
   end
 
   def test_win_when_close_mine
-    square = Square.new
-    assert(!square.win?)
-    square.put_mine
-    assert(square.win?)
+    assert(!@square.win?)
+    @square.put_mine
+    assert(@square.win?)
   end
 
   def test_mark_close
-    square = Square.new
-    assert_equal(' ', square.get_mark)
+    assert_equal(' ', @square.get_mark)
   end
 
   def test_mark_open
-    square = Square.new
-    square.open
-    assert_equal('0', square.get_mark)
-    square = Square.new
-    square.put_mine
-    square.open
-    assert_equal('*', square.get_mark)
+    @square.open
+    assert_equal('0', @square.get_mark)
+
+    @square = Square.new
+    @square.put_mine
+    @square.open
+    assert_equal('*', @square.get_mark)
   end
 end
