1c87623fbbb605a9eae38a35a220fb9521b0f03a
Dongkuk <dongkuk5411@nhnnext.org>
2014-01-06 02:14:13 +0900

    2x2 그리드 스펙 작성;

--- spec/models/grid_spec.rb
+++ spec/models/grid_spec.rb
@@ -2,12 +2,12 @@
 require_relative '../spec_helper'
 require 'minesweeper/grid'
 
-describe Grid do
+describe '1x1 그리드' do
   before :each do
     @grid = Grid.new
   end
 
-  context '1x1 그리드에서 셀이 마인인 경우,' do
+  context '셀이 마인인 경우,' do
     it '그냥 이긴다.' do
       cell = double('Cell')
       cell.stub(:mine?) { true }
@@ -16,7 +16,7 @@ describe Grid do
     end
   end
 
-  context '1x1 그리드에서 셀이 마인이 아닌 경우,' do
+  context '셀이 마인이 아닌 경우,' do
     it '오픈 안 하면 이긴게 아니다.' do
       cell = double('Cell')
       cell.stub(:mine?) { false }
@@ -27,8 +27,50 @@ describe Grid do
     it '오픈하면 이긴다.' do
       cell = double('Cell')
       cell.should_receive(:open)
-      @grid.should_receive(:cells) {[cell]}
+      @grid.should_receive(:cells) { [cell] }
       @grid.open
+    end
+  end
+end
+
+describe '2x2 그리드' do
+  context '마인이 0개인 경우,' do
+    it '오픈 안 하면 이긴게 아니다.' do
+
+    end
+
+    it '하나를 오픈하면 이긴다.' do
+
+    end
+  end
+
+  context '마인이 1, 2개인 경우,' do
+    it '마인을 오픈하면 진다.' do
+
+    end
+
+    it '마인이 아닌걸 오픈하면 아무일도 없다.' do
+
+    end
+
+    it '마인이 아닌걸 전부 오픈하면 이긴다.' do
+
+    end
+  end
+
+  context '마인이 3개인 경우,' do
+    it '마인을 오픈하면 진다.' do
+
+    end
+
+    it '마인이 아닌걸 오픈하면 이긴다.' do
+
+    end
+  end
+
+  context '마인이 4개인 경우,' do
+    it '그냥 이긴다.' do
+
     end
   end
 end
